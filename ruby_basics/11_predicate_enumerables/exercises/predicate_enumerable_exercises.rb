ca19ac7063944ff7eac4e7272b57f65ac6c4aa4f
Carter <carter.hemington@gmail.com>
2023-03-21 10:55:06 -0400

    updated predicate enumerables exercise, complete include?, any? and all?

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -1,13 +1,22 @@
 def coffee_drink?(drink_list)
   # use #include? to return true when the drink_list (array) contains the string "coffee" or "espresso"
+  drink_list.include?("coffee")|| drink_list.include?("espresso")
+  # tried grouping the terms, however include? only searches for one thing
+  # can't put the evaluations one after the other as only one would be caught
+  # therefore, all that is left is to set them up as an OR statement
 end
 
 def correct_guess?(guess_list, answer)
   # use #any? to return true when any element of the guess_list (array) equals the answer (number)
+  guess_list.any?(answer)
+  # any doesn't need a false condition built in for an empty array, it says no elements match code block therefore false
 end
 
 def twenty_first_century_years?(year_list)
   # use #all? to return true when all of the years in the year_list (array) are between 2001 and 2100
+  # return false if year_list.empty?
+  # removed the above as this function doesn't need to catch this edge case
+  year_list.all? {|year| year.between?(2001, 2100)}
 end
 
 def correct_format?(word_list)
